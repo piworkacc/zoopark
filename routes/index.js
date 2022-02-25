625b58ece5c17fa62f0aee5b78e1eb79b032bdd2
Artem <artemivlev@yandex.ru>
2022-02-25 14:42:30 +0300

    добавлена отрисовка животных с базы

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {TarifGrid,PeopleTypes} = require('../db/models')
+const {TarifGrid, Animal} = require('../db/models')
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const tarif = await TarifGrid.findAll({include:  'PeopleType'}, {raw:true})
+  const animals = await Animal.findAll()
   console.log(tarif.PeopleType)
-  res.render('index', { tarif });
+  res.render('index', { tarif, animals });
 });
 
 module.exports = router;
