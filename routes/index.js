d53d6b67ffaf117ccfa643ab5b936252abba6341
Artem <artemivlev@yandex.ru>
2022-02-25 13:32:44 +0300

    Добавлено отображение тарифной сетки из базы

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
 const router = express.Router();
+const {TarifGrid,PeopleTypes} = require('../db/models')
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', async (req, res, next) => {
+  const tarif = await TarifGrid.findAll({include:  'PeopleType'}, {raw:true})
+  console.log(tarif.PeopleType)
+  res.render('index', { tarif });
 });
 
 module.exports = router;
